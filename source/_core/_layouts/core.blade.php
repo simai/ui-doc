01b1c6324bf8f98091645f7598dcb5cc3b6f0f60
Burundug <iburundugi@gmail.com>
2025-10-23 11:50:57 +0300

    init

--- source/_core/_layouts/core.blade.php
+++
@@ -1,9 +0,0 @@
-@php
-$hasSha = $page->sha ?? 'latest';
-@endphp
-<link rel="preconnect" href="https://cdn.jsdelivr.net" crossorigin>
-<script>
-    window.sfPath = `https://cdn.jsdelivr.net/gh/simai/ui@${@json($hasSha)}/distr/`;
-</script>
-<script src="{{'https://cdn.jsdelivr.net/gh/simai/ui@'. $hasSha . '/distr/core/js/core.js'}}"></script>
-<link rel="stylesheet" href="{{'https://cdn.jsdelivr.net/gh/simai/ui@'. $hasSha . '/distr/core/css/core.css'}}"/>
